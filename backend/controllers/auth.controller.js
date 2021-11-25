0c14437e907d123cf8d0e3aba51ee66394a12655
Lucas Oliveira <purzbbq@gmail.com>
2021-11-25 13:42:00 +0100

    login & logout middlewares + routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const sql = require("../config/db");
 require('dotenv').config({path:'./config/.env'})
 
-
+// Create a user
 exports.create = (req, res, next) => {
     // Validate request
     if (!req.body) {
@@ -34,3 +34,45 @@ sql.query(sqlQuery,user,(err, result) => {
 })
  })}; 
  
+ ///////////////
+ /*  TOKEN   */
+ //////////////
+ // durée du token (3jours)
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+// fonction qui créer le token
+ const createToken = (id) => {
+   return jwt.sign({id}, process.env.JWT_SECRET,{
+     expiresIn :maxAge
+   })
+ };
+
+ 
+  // Login
+  exports.login = (req, res, next) => {
+    // on viens trouver l'utilisateur dans la base de données
+    const email = req.body.email;
+    const sqlQuery = `SELECT * FROM users WHERE email = '${email}'`
+    sql.query(sqlQuery,async (err, data) => {
+      if (err) {
+        res.status(404).json({error: err})
+      } else {
+        const match = await bcrypt.compare(req.body.password, data[0].password)
+        if(!match){
+          res.status(200).json({error:true, message:"Mot de passe incorrect"})
+        }
+        const token = createToken(data[0].id);
+            res.cookie('jwt', token, {httpOnly: true, maxAge:maxAge});
+            res.status(200).json({
+              userId: data[0].id
+            });
+      }
+      })
+    };
+
+
+
+module.exports.logout =(req, res) =>{
+  console.log('deconnexion');
+  res.cookie('jwt','',{maxAge: 1})
+  res.redirect('/');
+}
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 /* const {requireUserAuth} = require('../middlewares/auth.middleware'); */
 
-// USER CRUD //
+// AUTH ROUTES //
     // Create a new user
     router.post("/register", authController.create);
+    // login
+   router.post("/login", authController.login);
+   // logout
+   router.get("/logout", authController.logout);
 
    
     module.exports = router;
