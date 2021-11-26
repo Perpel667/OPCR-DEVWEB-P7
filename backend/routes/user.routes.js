7183365a6173428f47c2983f677ca3d9749887b5
Lucas Oliveira <purzbbq@gmail.com>
2021-11-26 18:17:55 +0100

    check user's permission & update middleware

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const userController = require("../controllers/user.controller");
-const auth = require("../middlewares/auth.middleware");
+const {Auth} = require("../middlewares/auth.middleware");
+const {requireUserAuth} = require("../middlewares/auth.middleware");
 
 // Get a specific user
-router.get("/:id",auth, userController.findOne);
+router.get("/:id",Auth, userController.findOne);
+router.put("/:id",requireUserAuth, userController.update);
 
 
 module.exports = router;
