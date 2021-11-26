28841534ea83049210f899fd21cfbe8826da993a
Lucas Oliveira <purzbbq@gmail.com>
2021-11-26 18:16:47 +0100

    user auth token verification created

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const sql = require("../config/db");
 
-module.exports = (req, res,next) => {
+module.exports.Auth = (req, res,next) => {
     const token = req.cookies.jwt;
     const sqlQuery = `SELECT id FROM users WHERE id = ?`
     if(!token){
@@ -21,4 +21,35 @@ module.exports = (req, res,next) => {
             })
         })
     }
+  }
+
+  module.exports.requireUserAuth = (req, res,next) => {
+    const sqlQuery = `SELECT id FROM users WHERE id = ?`
+    const token = req.cookies.jwt;
+    sql.query(sqlQuery,req.params.id, (err, data) => {
+        if (err) {
+            console.log("error: ", err);
+            res.status(404).json({err: err});
+            return;
+          }
+          if (data == 0) {
+            // not found User with the id
+            res.status(200).json({message : "aucun utilisateur trouvé avec cet id"})
+            return;
+          } else {
+            jwt.verify(token,process.env.JWT_SECRET,async (err,decodedToken)=>{
+                if(err){
+                    console.log("mauvais token");
+                }else{
+                   if(decodedToken.id == data[0].id){
+                       next();
+                   }else{
+                    res.status(404).send({
+                        message: `Vous n'êtes pas autoriser a faire cette action.`
+                      });
+                   }
+                }
+            })
+          }
+    })
   }
