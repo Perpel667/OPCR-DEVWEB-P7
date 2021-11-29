aca11ad9a12f683ca4724d4ec23e43c4e5714b56
Lucas Oliveira <purzbbq@gmail.com>
2021-11-29 12:19:44 +0100

    get all posts route

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -4,6 +4,8 @@ const {Auth} = require("../middlewares/auth.middleware");
 
 // Crete a post
 router.post("/",Auth,PostController.create);
+// get All posts
+router.get("/",Auth,PostController.getAllPosts);
 
 
 module.exports = router;
