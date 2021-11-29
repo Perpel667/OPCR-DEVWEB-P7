89fd7591bbea8c5f4d42ed7eb3ff9ebfcada4190
Lucas Oliveira <purzbbq@gmail.com>
2021-11-29 14:07:58 +0100

    modify post route

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const PostController  = require("../controllers/post.controller");
 const {Auth} = require("../middlewares/auth.middleware");
+const {requireAuth} = require("../middlewares/auth.middleware");
 
 // Crete a post
 router.post("/",Auth,PostController.create);
 // get All posts
 router.get("/",Auth,PostController.getAllPosts);
+// update a post
+router.put("/:id",requireAuth,PostController.updatePost);
 
 
 module.exports = router;
