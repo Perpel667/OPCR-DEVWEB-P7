be49e18034ec05a11d59915e3b89791331bbac62
Lucas Oliveira <purzbbq@gmail.com>
2021-11-30 18:00:58 +0100

    comment route

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use('/api/', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/post',postRoutes);
-app.use('/api/post',commentRoutes);
+app.use('/api/comment',commentRoutes);
 
 
 
