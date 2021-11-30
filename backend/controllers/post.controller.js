8d8f2e5729f721f1657fc0a40ea7f7659efa7c34
Lucas Oliveira <purzbbq@gmail.com>
2021-11-30 15:04:52 +0100

    create post/update/delete now work with imgs

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,45 +1,47 @@
 const sql = require("../config/db");
 const jwt = require('jsonwebtoken');
+const fs = require("fs"); //
 
 
-// create a post 
-exports.create = (req, res, next) => {
-    let file = req;
+   exports.create = (req, res, next) => {
+    let { body, file } = req;
     if (!file) delete req.body.image_url;
-    // Validate request
-    if (!req.body) {
-     res.status(400).send({
-       message: "Veuillez renseignez les champs requis !"
-     });
-   }
-  // get userid from jwt decoded token in cookies
+    // get userid from jwt decoded token in cookies
    let userid;
-    const token = req.cookies.jwt;
-    jwt.verify(token,process.env.JWT_SECRET,(err,decodedToken) =>{
-        if(err){
-            res.status(200).json({message: "no token"})
-        }
-      userid = decodedToken.id
-    })
-
-    //create the post object
-   const post =  ({
-    message : req.body.message,
-    user_id : userid,
-    date : new Date()
-  });
-    sql.query("INSERT INTO post SET ?", post, (err, result) => {
+   const token = req.cookies.jwt;
+   jwt.verify(token,process.env.JWT_SECRET,(err,decodedToken) =>{
+       if(err){
+           res.status(200).json({message: "no token"})
+       }
+     userid = decodedToken.id
+   })
+   // declare body with user_id we picked up before
+    body = {
+      message : req.body.message,
+      user_id : userid,
+    };
+  // if there is an image then 
+  if (file) {
+    const sqlInsertImage = `INSERT INTO post (message,image_url,user_id) VALUES ("${body.message}","${file.filename}", ${userid})`;
+    sql.query(sqlInsertImage, (err, result) => {
       if (err) {
-        console.log("error: ", err);
-        res.status(400).json({err:err});
-        return;
+        res.status(404).json({ err });
+        throw err;
       }
-       post_id = result.insertId;
-      console.log("Post créer: ", { id: result.insertId, ...post });
-      res.status(201).json({ id: result.insertId, ...post });
+      res.status(200).json(result);
     });
-  
-   }; 
+    // no image ? then 
+  } else {
+    const sqlQuery = `INSERT INTO post SET ?`
+    sql.query(sqlQuery, body,(err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(404).json({ err });
+      }
+      res.status(200).json(result);
+    })
+  }
+  };
 
  // Get all posts from the database
 exports.getAllPosts = (req, res) => {
@@ -60,42 +62,72 @@ exports.getAllPosts = (req, res) => {
         });
       };
   
-    
- // update a post
-exports.updatePost = (req, res) => {
-        // Validate request
-        if (!req.body) {
-         res.status(400).send({
-           message: "Veuillez renseignez les champs requis !"
-         });
-       }
-       const post = ({
-        message : req.body.message,
-        date : new Date()
+
+exports.updatePost = (req, res, next) => {
+  let { body, file } = req;
+  if (!file) delete req.body.image_url;
+ // declare body with user_id we picked up before
+  body = {
+    message : req.body.message,
+  };
+  const postId = req.params.id;
+// if there is an image then 
+sql.query(`SELECT * FROM post WHERE id = ${postId}`,(err, data) =>{
+  if (err) {
+    console.log(err);
+    res.status(404).json({ err })
+  }
+  if(data.length){
+    if (file) {
+      // get previous Image & Delete it from server.
+      const previousImg = data[0].image_url;
+       fs.unlink(`./images/posts/${previousImg}`, err =>{
+        if(err){
+          console.log(err);
+        } console.log("Image supprimée du serveur.");
+      }) 
+      const sqlInsertImage = `UPDATE post SET message = '${req.body.message}', image_url = '${file.filename}' WHERE id = ${req.params.id}`;
+      sql.query(sqlInsertImage, (err, result) => {
+        if (err) {
+          res.status(404).json({ err });
+          throw err;
+        }
+        res.status(200).json(result);
       });
-      const postId = req.params.id;
-      const sqlQuery = `UPDATE post SET ? WHERE id = ${postId}`;
-      sql.query(`SELECT * FROM post WHERE id = ${postId}`,(err, data)=>{
-          if (err) {
-              console.log(err);
-              res.status(400).json({err: err})
-          }
-          if(data.length){
-              sql.query(sqlQuery,post,(err,result)=>{
-                  if(err){
-                      console.log(err);
-                      res.status(400).json({err:err})
-                  }
-                  res.status(200).json(result)
-              })
-          } else{
-              res.status(404).json({message:"Aucun post trouver avec cet id"})
-          }
+      // no image ? then 
+    } else {
+      const sqlQuery = `UPDATE post SET ? WHERE id = ${postId}`
+      sql.query(sqlQuery,body,(err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(404).json({ err });
+        }
+        res.status(200).json(result);
       })
+    }
+  }else {
+    res.status(404).json({message:"Aucun post trouver avec cet id."})
+  }
+})
+
 };
 
 // delete post
 exports.deletePost = (req, res) => {
+  sql.query(`SELECT * FROM post WHERE id = ${req.params.id}`,(err,data)=>{
+    if (err) {
+      console.log(err);
+      res.status(400).json({ err : err})
+    } else if(data[0].image_url) {
+      fs.unlink(`./images/posts/${data[0].image_url}`, err=>{
+        if (err) {
+          console.log(err);
+        }else{
+          console.log("Image supprimée du serveur");
+        }
+      })
+    }
+  })
      const sqlQuery = `DELETE FROM post WHERE id = ?`
         sql.query(sqlQuery, req.params.id, (err, result) => {
           if (err) {
@@ -107,10 +139,7 @@ exports.deletePost = (req, res) => {
             res.status(404).json({message: "aucun post trouver avec cet id."});
             return;
           }
-      
           console.log(`le post avec l'id ${req.params.id} a été supprimé`);
           res.status(200).json({message:"Post supprimé"});
         });
 }
-
-      
