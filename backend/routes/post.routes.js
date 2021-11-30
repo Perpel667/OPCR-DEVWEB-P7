b358a2c3ff9436e5810f22bcb1cfe2edd20a292c
Lucas Oliveira <purzbbq@gmail.com>
2021-11-30 11:16:40 +0100

    add multer to create post route

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const PostController  = require("../controllers/post.controller");
 const {Auth} = require("../middlewares/auth.middleware");
 const {requireAuth} = require("../middlewares/auth.middleware");
+const upload = require("../middlewares/multer.config")
 
 // Crete a post
-router.post("/",Auth,PostController.create);
+router.post("/",upload.single("image_url"),Auth,PostController.create);
 // get All posts
 router.get("/",Auth,PostController.getAllPosts);
 // update a post
