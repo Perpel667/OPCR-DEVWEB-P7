fad752b44c1e412a0a8cea1292a3bc775a7a36ad
Lucas Oliveira <purzbbq@gmail.com>
2021-12-01 10:35:51 +0100

    refacto + update & delete comment

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,10 +1,9 @@
 const sql = require("../config/db");
 const jwt = require('jsonwebtoken');
 
-exports.createComment =(req, res,next) => {
-
-    // get user's id from token
-    let userid;
+// get user's id from token
+let userid;
+const getUserId = (req, res) =>{
    const token = req.cookies.jwt;
    jwt.verify(token,process.env.JWT_SECRET,(err,decodedToken) =>{
        if(err){
@@ -12,6 +11,11 @@ exports.createComment =(req, res,next) => {
        }
      userid = decodedToken.id
    })
+}
+
+exports.createComment =(req, res,next) => {
+    
+    getUserId(req,res);
    // get postId from params
     const postId = req.params.id;
 
@@ -41,23 +45,16 @@ exports.getAllComment =(req, res,next) => {
 
 }
 exports.updateComment =(req, res,next) => {
-     // get user's id from token
-     let userid;
-     const token = req.cookies.jwt;
-     jwt.verify(token,process.env.JWT_SECRET,(err,decodedToken) =>{
-         if(err){
-             res.status(200).json({message: "no token"})
-         }
-       userid = decodedToken.id
-     })
+
+     getUserId(req,res);
    // get postId from params
     const commentId = req.params.id;
+
     sql.query(`SELECT * FROM comments WHERE id = ${commentId}`,(err,data) =>{
         if(err){
             console.log(err);
         }
         if(data[0].user_id == userid){
-            console.log("oui");
            const sqlUpdateComment = `UPDATE comments SET message='${req.body.message}' WHERE id = ${commentId} AND user_id = ${userid}`
            sql.query(sqlUpdateComment,(err,result)=>{
                if (err) {
@@ -70,4 +67,29 @@ exports.updateComment =(req, res,next) => {
             res.status(403).json({message:"vous n'etes pas l'auteur de ce commentaire."})
         }
     })
+}
+
+exports.deleteComment =(req, res,next) => {
+
+    getUserId(req,res);
+  // get postId from params
+   const commentId = req.params.id;
+
+   sql.query(`SELECT * FROM comments WHERE id = ${commentId}`,(err,data) =>{
+       if(err){
+           console.log(err);
+       }
+       if(data[0].user_id == userid){
+          const sqlDeleteComment = `DELETE FROM comments WHERE id = ${commentId}`
+          sql.query(sqlDeleteComment,(err,result)=>{
+              if (err) {
+                  res.status(404).json({ err: err})
+              } else {
+                  res.status(201).json(result)
+              }
+          })
+       } else {
+           res.status(403).json({message:"vous n'etes pas l'auteur de ce commentaire."})
+       }
+   })
 }
