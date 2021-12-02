53a3dfa325d3055e05d2037328d3ac76a86df901
Lucas Oliveira <purzbbq@gmail.com>
2021-12-02 11:27:28 +0100

    renamed login => register

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -1,21 +1,27 @@
 import './register.scss'
 
 export default function Register() {
-    return (
-        <div className="login">
-            <div className="loginWrapper">
-                <div className="loginLeft">
-                    <img src="/assets/logos/icon-left-font-monochrome-black.svg" alt="" className="loginLogo" />
-                    <span className="loginDesc">Bienvenue sur le réseau social de l'entreprise</span>
+
+ return (
+        <div className="register">
+            <div className="registerWrapper">
+                <div className="registerLeft">
+                    <img src="/assets/logos/icon-left-font-monochrome-black.svg" alt="" className="registerLogo" />
+                    <span className="registerDesc">Bienvenue sur le réseau social de l'entreprise</span>
                 </div>
-                <div className="loginRight">
-                    <div className="loginBox">
-                        <input type="email" placeholder="Email" className="loginInput" />
-                        <input placeholder="Nom" className="loginInput" />
-                        <input placeholder="Prenom" className="loginInput" />
-                        <input  placeholder="Mot de passe" className="loginInput" />
-                        <input  placeholder="Confirmer le mot de passe" className="loginInput" />
-                        <button className="loginButton">S'inscrire</button>
+                <div className="registerRight">
+                    <div className="registerBox">
+                        <input type="email" placeholder="Email" className="registerInput" />
+                        
+                        <input placeholder="Nom" className="registerInput" />
+                        
+                        <input placeholder="Prenom" className="registerInput" />
+                        
+                        <input  placeholder="Mot de passe" className="registerInput" />
+                       
+                        <input  placeholder="Confirmer le mot de passe" className="registerInput" />
+                        
+                        <button className="registerButton">S'inscrire</button>
                     </div>
                 </div>
             </div>
