55d13a8bb4fe289383c16f2b64393d85b12a630b
Lucas Oliveira <purzbbq@gmail.com>
2021-12-08 16:12:16 +0100

    config redux and create a store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+// dev tools
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+
+
+
+const store = createStore(
+  rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))
+)
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  ,
   document.getElementById('root')
 );
