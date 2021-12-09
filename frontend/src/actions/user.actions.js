3df2a693f98a6232d02456f229b0f9df08595390
Lucas Oliveira <purzbbq@gmail.com>
2021-12-09 10:46:19 +0100

    add withCredentials ( that was the probem )

--- frontend/src/actions/user.actions.js
+++ frontend/src/actions/user.actions.js
@@ -6,7 +6,9 @@ export const GET_USER = "GET_USER";
 export const getUser = (uid) => {
     return (dispatch) => {
         return axios
-        .get(`http://localhost:5000/api/user/${uid}`)
+        .get(`http://localhost:5000/api/user/${uid}`,{
+            withCredentials: true
+        })
         .then((res) => {
             dispatch({ type: GET_USER, payload: res.data });
         })
