fcb73754f9625ad892de4ceadb14caed22057a02
Lucas Oliveira <purzbbq@gmail.com>
2021-12-10 15:59:18 +0100

    Starting profile page

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -1,11 +1,26 @@
 import Navbar from '../../Components/navbar/Navbar';
+import { useSelector } from 'react-redux';
 import './profile.scss';
+import profileBackground from '../../assets/images/profile-background2.jpeg';
 
 
 export default function Profile() {
+
+    const userData = useSelector((state) => state.userReducer)
     return (
         <div>
             <Navbar />
+            <div className="profile">
+          <div className="profileTop">
+            <div className="profileTitle">
+                <img className="profileCoverImg" src={profileBackground} alt="" />
+            <img src={`http://localhost:5000/api/${userData.image}`} alt="" className="profileUserImg" />
+            </div>
+          </div>
+          <div className="profileBottom">
+              <button>Modifier votre mot de passe</button>
+          </div>
+        </div>
         </div>
     )
 }
