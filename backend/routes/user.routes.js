8e1363664a4a4f75ee5df7e253712e965ad206d0
Lucas Oliveira <purzbbq@gmail.com>
2021-12-14 11:22:13 +0100

    add modify profil picture route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,11 +2,16 @@ const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 const {Auth} = require("../middlewares/auth.middleware");
 const {requireUserAuth} = require("../middlewares/auth.middleware");
+const upload = require("../middlewares/multer.config")
 
 // Get a specific user
 router.get("/:id",Auth, userController.findOne);
+// modify a specific user
 router.put("/:id",requireUserAuth, userController.update);
+// Delete a user
 router.delete("/:id",requireUserAuth, userController.deleteUser);
+// modify profil picture
+router.put("/picture/:id",requireUserAuth,upload.single("image"), userController.updateProfilePicture);
 
 
 module.exports = router;
