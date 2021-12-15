62956150a0fad6d52c8356ee1a559a67da344bcd
Lucas Oliveira <purzbbq@gmail.com>
2021-12-15 10:13:47 +0100

    add the logout button

--- frontend/src/Components/navbar/Navbar.jsx
+++ frontend/src/Components/navbar/Navbar.jsx
@@ -3,9 +3,12 @@ import { useState } from 'react';
  import { useDispatch } from 'react-redux';
  import { useSelector } from 'react-redux';
  import { useEffect } from 'react';
+ import axios from "axios";
 import { getUser } from "../../actions/user.actions"
 import { AiOutlineHome } from "react-icons/ai";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 import { Sling as Hamburger } from 'hamburger-react'
+
 
 export default function Navbar() {
 
@@ -23,12 +26,33 @@ export default function Navbar() {
     const userData = useSelector((state) => state.userReducer)
 
     
+    // logout handle
+    const handleLogout = (e) =>{
+        // redirect to the login page
+         window.location = '/login';
+
+        // logout
+        axios({
+            method: "GET",
+            url:`http://localhost:5000/api/logout`,
+            withCredentials: true,
+        })
+       .then(response =>{
+            return response.data
+       })
+       .catch(error =>{
+           console.log(error.response.data);
+           return
+       })
+    }
+
     return (
         <div className="navbarContainer">
             <div className="navbarLeft">
               <img src="/assets/logos/icon-left-font-monochrome-white.svg" alt="logo Groupomania" className="logo" />  
             </div>
             <div className="navbarRight">
+                {/* home */}
             <div className="navbarLink navbarIcon home" onClick={(e) => window.location = "/"}>
                 <AiOutlineHome />
                 </div>
@@ -38,11 +62,17 @@ export default function Navbar() {
                     <div className="hamburgerMenu">
                         <p className="hamburgerMenuLinks" onClick={(e)=> window.location = "/profile"}>Mon Profil</p>
                         <p className="hamburgerMenuLinks" onClick={(e)=> window.location = "/"}>Fil d'actualité</p>
+                        <p className="hamburgerMenuLinks" onClick={handleLogout}>Se deconnecter</p>
                     </div>
                      }
                 
                 </div>
+                {/* profile */}
             <img src={`http://localhost:5000/api/${userData.image}`} alt="profil" className="profilePicture navbarLink" onClick={(e) => window.location = "/profile"} />
+            {/* logout */}
+            <div className="navbarLink navbarIcon logout" onClick={handleLogout}>
+                <RiLogoutBoxRLine />
+                </div>
             </div>
         </div>
     )
