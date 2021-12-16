fc680c6e9077d773d6661b79e439b78a13ea69b1
Lucas Oliveira <purzbbq@gmail.com>
2021-12-16 17:52:37 +0100

    start home page

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import './home.scss'
 import Navbar from "../../Components/navbar/Navbar";
+import Feed from '../../Components/Feed/Feed';
 
 export default function Home() {
     return (
         <div>
           <Navbar /> 
+          <div className="homeContainer">
+            <Feed />
+          </div>
         </div>
     )
 }
