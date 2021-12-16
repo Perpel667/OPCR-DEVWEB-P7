044e11eb9ff17c5ba93a1049d8e3b0f7510896e0
Lucas Oliveira <purzbbq@gmail.com>
2021-12-16 17:53:28 +0100

    starting the feed component

---
+++ frontend/src/Components/Feed/Feed.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPosts } from '../../actions/post.actions';
+import Post from '../Post/Post';
+import Share from '../Share/Share';
+import './feed.scss';
+
+export default function Feed() {
+    const [loadPost,setLoadPost] = useState(true);
+    const dispatch = useDispatch();
+
+    const posts = useSelector((state) => state.postReducer);
+
+    useEffect(()=>{
+        if(loadPost){
+            dispatch(getPosts());
+            setLoadPost(false)
+        }
+    },[loadPost, dispatch])
+    return (
+        <div className="Feed">
+            <div className="feedWrapper">
+                <Share />
+                <ul>
+                    {(posts[0]) && 
+                    posts.map((post)=>{
+                        return <Post post={post} key={post.id} />
+                    })
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
