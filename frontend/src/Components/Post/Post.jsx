5213a1184df5f0cf404debeb332ae50731f11089
Lucas Oliveira <purzbbq@gmail.com>
2021-12-16 19:22:07 +0100

    starting post card

--- frontend/src/Components/Post/Post.jsx
+++ frontend/src/Components/Post/Post.jsx
@@ -1,19 +1,40 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import { FaCircleNotch } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import { FiMoreVertical } from "react-icons/fi";
+
 import './post.scss';
 
 export default function Post({post}) {
 
     const [isLoading,setIsLoading] = useState(true);
      
+    useEffect(() =>{ post && setIsLoading(false)
+
+    },[post])
     return (
-        <li className="card-container" key={post.id}>
+        <div className="card-container" key={post.id}>
             {isLoading ? (
                 <FaCircleNotch class="icon-spin"/>
             ): (
-                <h2>oui</h2>
+                <div className="postWrapper">
+                    <div className="postTop">
+                        <div className="postTopLeft">
+                            <img src={`http://localhost:5000/api/${post.profilePicture}`} alt="" className="postProfilePicture"/>
+                            <span className="postUsername">{post.name} {post.firstname}</span>
+                            <span className="postDate">{post.date}</span>
+                        </div>
+                        <div className="postTopRight">
+                        <FiMoreVertical />
+                    </div>
+                    </div>
+                    <div className="postCenter">
+                    <span className="postText">{post?.message}</span>
+                    <img src={`http://localhost:5000/api/images/posts/${post.image_url}`} alt="" className="postImg"/>
+                </div>
+                    
+                </div>
             )}
-        </li>
+        </div>
     )
 }
