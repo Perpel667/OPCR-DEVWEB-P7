7e0ebdb5924f8b1f7cbea50a672051cb6da54c21
Lucas Oliveira <purzbbq@gmail.com>
2021-12-17 14:54:55 +0100

    add moment to show date from ago

--- frontend/src/Components/Post/Post.jsx
+++ frontend/src/Components/Post/Post.jsx
@@ -1,9 +1,11 @@
 import {useEffect, useState} from 'react';
 import { FaCircleNotch } from "react-icons/fa";
-import { useSelector } from "react-redux";
 import { FiMoreVertical } from "react-icons/fi";
 
 import './post.scss';
+import moment from 'moment';
+import 'moment/locale/fr';
+moment.locale('fr');
 
 export default function Post({post}) {
 
@@ -15,14 +17,17 @@ export default function Post({post}) {
     return (
         <div className="card-container" key={post.id}>
             {isLoading ? (
-                <FaCircleNotch class="icon-spin"/>
+                <FaCircleNotch className="icon-spin"/>
             ): (
                 <div className="postWrapper">
                     <div className="postTop">
                         <div className="postTopLeft">
                             <img src={`http://localhost:5000/api/${post.profilePicture}`} alt="" className="postProfilePicture"/>
-                            <span className="postUsername">{post.name} {post.firstname}</span>
-                            <span className="postDate">{post.date}</span>
+                            <div className="postTopUserAndDate">
+                               <span className="postUsername">{post.name} {post.firstname}</span> 
+                               <span className="postDate">{moment(post.date).fromNow()}</span>
+                            </div>
+                            
                         </div>
                         <div className="postTopRight">
                         <FiMoreVertical />
