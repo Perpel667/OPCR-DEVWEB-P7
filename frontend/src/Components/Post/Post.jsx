8995787faa8e38e1bc7c64309598f0020ba27809
Lucas Oliveira <purzbbq@gmail.com>
2021-12-20 17:39:53 +0100

    add like function but not working as i wish

--- frontend/src/Components/Post/Post.jsx
+++ frontend/src/Components/Post/Post.jsx
@@ -1,6 +1,8 @@
 import {useEffect, useState} from 'react';
 import { FaCircleNotch } from "react-icons/fa";
 import { FiMoreVertical } from "react-icons/fi";
+import { FcLikePlaceholder } from "react-icons/fc";
+import axios from "axios";
 
 import './post.scss';
 import moment from 'moment';
@@ -10,6 +12,27 @@ moment.locale('fr');
 export default function Post({post}) {
 
     const [isLoading,setIsLoading] = useState(true);
+
+    const [like,setLike] = useState(post.Likes);
+    const [isLiked,setIsLiked] = useState(false);
+
+
+const likeHandler = () => {
+    setLike(isLiked ? like-1 : like+1)
+    setIsLiked(!isLiked)
+    axios({
+        method: "PATCH",
+        url:`http://localhost:5000/api/post/${post.id}/likes`,
+        withCredentials: true,
+    })
+   .then(response =>{
+        return response.data
+   })
+   .catch(error =>{
+       console.log(error.response.data);
+       return
+   })
+}
      
     useEffect(() =>{ post && setIsLoading(false)
 
@@ -37,7 +60,15 @@ export default function Post({post}) {
                     <span className="postText">{post?.message}</span>
                     <img src={`http://localhost:5000/api/images/posts/${post.image_url}`} alt="" className="postImg"/>
                 </div>
-                    
+                <div className="postBottom">
+                    <div className="postBottomLeft">
+                    <FcLikePlaceholder className="likeIcon" onClick={likeHandler} />{like}
+                        <span className="postLikeCounter"></span>
+                    </div>
+                    <div className="postBottomright">
+                        <span className="postCommentText">{post.Commentaires} commentaires</span>
+                    </div>
+                </div>
                 </div>
             )}
         </div>
