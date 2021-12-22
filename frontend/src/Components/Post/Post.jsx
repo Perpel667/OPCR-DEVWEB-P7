63c2e73a4b3ad6c75c93f5a7b27aa2e8fb876050
Lucas Oliveira <purzbbq@gmail.com>
2021-12-22 15:45:51 +0100

    moved the likes function into a component

--- frontend/src/Components/Post/Post.jsx
+++ frontend/src/Components/Post/Post.jsx
@@ -1,39 +1,23 @@
 import {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
 import { FaCircleNotch } from "react-icons/fa";
 import { FiMoreVertical } from "react-icons/fi";
-import { FcLikePlaceholder } from "react-icons/fc";
-import axios from "axios";
+/* import axios from "axios"; */
 
 import './post.scss';
 import moment from 'moment';
 import 'moment/locale/fr';
+import LikeButton from './LikeButton';
 moment.locale('fr');
 
-export default function Post({post}) {
+export default function Post({post,userData}) {
+
 
     const [isLoading,setIsLoading] = useState(true);
-
-    const [like,setLike] = useState(post.Likes);
-    const [isLiked,setIsLiked] = useState(false);
+    const postLiked = useSelector((state) => state.likesReducer);
 
 
-const likeHandler = () => {
-    setLike(isLiked ? like-1 : like+1)
-    setIsLiked(!isLiked)
-    axios({
-        method: "PATCH",
-        url:`http://localhost:5000/api/post/${post.id}/likes`,
-        withCredentials: true,
-    })
-   .then(response =>{
-        return response.data
-   })
-   .catch(error =>{
-       console.log(error.response.data);
-       return
-   })
-}
-     
+
     useEffect(() =>{ post && setIsLoading(false)
 
     },[post])
@@ -62,7 +46,7 @@ const likeHandler = () => {
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                    <FcLikePlaceholder className="likeIcon" onClick={likeHandler} />{like}
+                    <LikeButton post={post} userData={userData} postLiked={postLiked} />
                         <span className="postLikeCounter"></span>
                     </div>
                     <div className="postBottomright">
