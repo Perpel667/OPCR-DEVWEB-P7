da3a452cfcbc2a326d7e31c8d8e252db550cb15e
Lucas Oliveira <purzbbq@gmail.com>
2021-12-27 11:38:18 +0100

    Create post works well but double when no picture

--- frontend/src/Components/Share/Share.jsx
+++ frontend/src/Components/Share/Share.jsx
@@ -1,28 +1,89 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import axios from "axios";
 import { GoFileMedia } from "react-icons/go";
 import './share.scss'
 
 
 export default function Share() {
     const userData = useSelector((state) => state.userReducer);
+
+    const [message,setMessage] = useState("");
+    const [picture,setPicture] = useState (null);
+
+    // set post picture handle
+    const HandlePostPicture = (e) =>{
+        function setPictureAsync() {
+          return new Promise(resolve => {
+              setPicture(e.target.files[0])
+              resolve(picture);
+          });
+        }
+        
+        async function getPictureAsync() {
+           await setPictureAsync();
+        }
+        getPictureAsync();
+    }
+
+    // New Post Handle
+    const handleNewPost = (e) =>{
+        if(picture === null){
+            axios({
+                method: "POST",
+                url: `http://localhost:5000/api/post/`,
+                data: message,
+                withCredentials: true,
+              })
+              .then(response =>{
+                  console.log(response);
+              })
+              .catch(error =>{
+                  console.log(error);
+              })
+        }
+        const data = new FormData();
+        data.append("message", message);
+        data.append("image_url", picture);
+    
+          axios({
+              method: "POST",
+              url: `http://localhost:5000/api/post/`,
+              data: data,
+              headers: { "Content-Type": "multipart/form-data" },
+              withCredentials: true,
+            })
+            .then(response =>{
+                console.log(response);
+            })
+            .catch(error =>{
+                console.log(error);
+            })
+    }
+    
+
     return (
         <div className="share">
-            <div className="shareWrapper">
+            <form className="shareWrapper" onSubmit={handleNewPost}>
                 <div className="shareTop">
                      <img src={`http://localhost:5000/api/${userData.image}`} alt="" className="shareProfilePicture"/>
-                    <input placeholder="Exprimez-vous" className="shareInput" />
+                    <input placeholder="Exprimez-vous" className="shareInput" onChange={(e) => setMessage(e.target.value)} value={message} />
                 </div>
                 <hr className="shareHr"/>
                 <div className="shareBottom">
                     <div className="shareOptions">
                         <div className="shareOption">
                         <GoFileMedia className="shareIcon"/>
-                            <span className="shareOptionText">Ajouter une photo</span>
+                            
+                            <input type="file" name="image" id="image" style={{display: "none"}} onChange={HandlePostPicture}/>
+                            <label htmlFor="image" >
+                                <span className="shareOptionText">Ajouter une photo</span>
+                            </label> 
                         </div>
                     </div>
-                    <button className="shareButton">Publier</button>
+                    <button className="shareButton" type="submit" >Publier</button>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
