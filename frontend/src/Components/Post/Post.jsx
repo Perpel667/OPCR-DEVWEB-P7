1f9060290b392b5bd1109a0fc85e972f87479657
Lucas Oliveira <purzbbq@gmail.com>
2021-12-27 14:58:41 +0100

    add postOption div to update or delete a post

--- frontend/src/Components/Post/Post.jsx
+++ frontend/src/Components/Post/Post.jsx
@@ -1,7 +1,9 @@
 import {useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
+import Modal from 'react-modal';
 import { FaCircleNotch } from "react-icons/fa";
 import { FiMoreVertical } from "react-icons/fi";
+import { FaTrashAlt,FaPencilAlt } from "react-icons/fa";
 /* import axios from "axios"; */
 
 import './post.scss';
@@ -10,11 +12,20 @@ import 'moment/locale/fr';
 import LikeButton from './LikeButton';
 moment.locale('fr');
 
+
+  Modal.setAppElement('#root');
+
 export default function Post({post,userData}) {
 
 
     const [isLoading,setIsLoading] = useState(true);
+    const [postOptions,setPostOptions] = useState(false);
+
+    function toggle() {
+        setPostOptions(wasOpened => !wasOpened);
+      }
     const postLiked = useSelector((state) => state.likesReducer);
+
 
 
 
@@ -28,6 +39,12 @@ export default function Post({post,userData}) {
             ): (
                 <div className="postWrapper">
                     <div className="postTop">
+                    {(postOptions && post.user_id === userData.id) &&  
+                    <div className="postOptions">
+                        <span className="postOptionsUpdate"><FaPencilAlt /></span>
+                        <span className="postOptionsDelete"><FaTrashAlt /></span>
+                    </div>
+                    }
                         <div className="postTopLeft">
                             <img src={`http://localhost:5000/api/${post.profilePicture}`} alt="" className="postProfilePicture"/>
                             <div className="postTopUserAndDate">
@@ -37,8 +54,7 @@ export default function Post({post,userData}) {
                             
                         </div>
                         <div className="postTopRight">
-                            {post.user_id === userData.id && <FiMoreVertical />}
-                        
+                            {post.user_id === userData.id && <FiMoreVertical onClick={toggle}/>}
                     </div>
                     </div>
                     <div className="postCenter">
