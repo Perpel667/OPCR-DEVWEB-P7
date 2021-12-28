02cbea4f457ff070a43882ba88babd3a89964c94
Lucas Oliveira <purzbbq@gmail.com>
2021-12-28 19:03:27 +0100

    add update post reducer

--- frontend/src/reducers/post.reducer.js
+++ frontend/src/reducers/post.reducer.js
@@ -1,4 +1,4 @@
-import { DELETE_POST, GET_POSTS } from "../actions/post.actions";
+import { DELETE_POST, GET_POSTS, UPDATE_POST } from "../actions/post.actions";
 
 const initialState = {};
 
@@ -8,6 +8,17 @@ export default function postReducer(state = initialState, action) {
             return action.payload;
         case DELETE_POST:
             return state.filter((post)=> post.id !== action.payload);
+        case UPDATE_POST:
+            return state.map((post) => {
+                if(post.id === action.payload.postId){
+                    return{
+                        ...post,
+                        data: action.payload.data
+                    }
+                } else {
+                    return post
+                }
+            })
         default:
             return state;
         
