86e29743ca5fe0ce60aa1e552797775509d40088
Lucas Oliveira <purzbbq@gmail.com>
2021-12-29 11:50:04 +0100

    create users actions & reducer to get all users

---
+++ frontend/src/actions/users.actions.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const GET_USERS = "GET_USERS";
+
+export const getUsers = () => {
+    return (dispatch) => {
+        return axios
+        .get(`http://localhost:5000/api/user/`,{
+            withCredentials: true
+        })
+        .then((res) => {
+            dispatch({ type: GET_USERS, payload: res.data });
+        })
+        .catch((err) => console.log(err));
+    };
+};
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import userReducer from "./user.reducer";
+import usersReducer from "./users.reducer";
 import postReducer from "./post.reducer";
 import commentsReducer from "./comments.reducer";
 
 
 export default combineReducers({
     userReducer,
+    usersReducer,
     postReducer,
     commentsReducer,
 });
