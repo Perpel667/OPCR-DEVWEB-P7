9eaf006041d65e689056ee1c2e57cbd60e697450
Lucas Oliveira <purzbbq@gmail.com>
2022-01-05 10:34:55 +0100

    secure route when not logged in

--- frontend/src/Components/Routes/index.jsx
+++ frontend/src/Components/Routes/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
 
 import Home from '../../Pages/Home/Home';
 import Register from '../../Pages/Register/Register';
 import Login from '../../Pages/Login/Login';
 import Profile from '../../Pages/Profile/Profile';
 
-export default function index() {
+export default function Index() {
+  const isAuthenticated = !!localStorage.getItem("userId");
     return (
         <Router>
         <Routes>
           <Route exact path="/login" element={<Login/>}/>
           <Route exact path="/register" element={<Register/>}/>
-          <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/profile" element={<Profile/>}/>
+          <Route path="/" element={isAuthenticated ? (<Home/>) : (<Navigate to="/login" />)}/>
+          <Route path="/profile" element={isAuthenticated ? (<Profile/>) : (<Navigate to="/login" />)}/>
         </Routes>
     </Router>
     )
