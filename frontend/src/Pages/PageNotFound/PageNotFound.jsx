a08348c1c7230765762549533cf62218e4fa7356
Lucas Oliveira <purzbbq@gmail.com>
2022-01-07 11:29:27 +0100

    add alt to img

--- frontend/src/Pages/PageNotFound/PageNotFound.jsx
+++ frontend/src/Pages/PageNotFound/PageNotFound.jsx
@@ -6,7 +6,7 @@ export default function PageNotFound() {
     return (
         <div className="pnf">
             <h1 className="pnf title-first">Oups !</h1>
-            <img src={Error} alt="" />
+            <img src={Error} alt="404 Error" />
             <h2 className="pnf title-second">LA PAGE N'EXISTE PAS</h2>
             <button className="return-btn" onClick={(e)=> window.location="/"}>Retour a l'acceuil</button>
         </div>
